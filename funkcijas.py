af8769dea0b3f39f1c338d5aa60644d0a2c4b6fa
indansm <118436727+indansm@users.noreply.github.com>
2023-04-14 14:11:10 +0300

    funkcijas gandriz pabeigtas, sākts GUI

---
+++ funkcijas.py
@@ -0,0 +1,38 @@
+# pat=0
+
+degv=5
+km=100
+kmvec=0
+kmjaun=1008546
+
+def nobraukums(kmvec,kmjaun):
+    kmvec-kmjaun
+    return 
+
+
+def paterins(degv,km):
+    # km=kmjaun-kmvec
+    pat=(degv*100)/km
+    return pat
+
+def vajagDeg(km, pat):
+    return (pat / 100) * km
+
+def pirkcena():
+    degv*cena
+    return
+def kmArDegv(pat, degv):
+    return (degv / (pat / 100))
+
+def BraucCena(Km, pat, cena):
+    degv = (pat / 100) * km
+    return degv * cena
+
+
+print(paterins(degv,km), " l/100")
+print(vajagDeg(200,5.5), "l")
+
+print(kmArDegv(5,5),"km")
+
+print(BraucCena(100,5,1.60))
+
--- index.py
+++ index.py
@@ -1,21 +1,47 @@
-# pat=0
+import tkinter as tk
+from tkinter import ttk
+from funkcijas import *
+# Create the main window
+root = tk.Tk()
+root.title("Degvielas Kalkulators")
 
-degv=5
-km=100
-kmvec=0
-kmjaun=1008546
+# Create the notebook widget for the tabs
+notebook = ttk.Notebook(root)
 
-def nobraukums(kmvec,kmjaun):
-    kmvec-kmjaun
-    return 
+# Create the tabs
+DegvielasPaterins = ttk.Frame(notebook)
+tab2 = ttk.Frame(notebook)
+tab3 = ttk.Frame(notebook)
 
+# Add the tabs to the notebook
+notebook.add(DegvielasPaterins, text="Degvielas patēriņs L/100")
+degv_ievade = tk.Label(DegvielasPaterins, text="Degvielas daudzums (L):")
+degv_ievade.grid(row=0, column=0)
 
-def paterins(degv,km):
-    # km=kmjaun-kmvec
-    pat=(degv*100)/km
-    return pat
-def pirkcena():
-    degv*cena
-    return
+degv = tk.Entry(DegvielasPaterins)
+degv.grid(row=0, column=1)
 
-print(paterins(degv,km))
+fuel_consumption_label = tk.Label(DegvielasPaterins, text="Nobrauktais KM:")
+fuel_consumption_label.grid(row=1, column=0)
+
+km = tk.Entry(DegvielasPaterins)
+km.grid(row=1, column=1)
+
+calculate_button = tk.Button(DegvielasPaterins, text="Aprēķināt", command=lambda:paterins(float(degv),float(km)))
+calculate_button.grid(row=2, column=0, columnspan=2)
+
+fuel_range_label = tk.Label(DegvielasPaterins, text="")
+fuel_range_label.grid(row=3, column=0)
+notebook.add(tab2, text="Tab 2")
+notebook.add(tab3, text="Tab 3")
+
+# Add some widgets to the tabs
+# tk.Label(tab1, text="This is Tab 1").pack(pady=10)
+tk.Label(tab2, text="This is Tab 2").pack(pady=10)
+tk.Label(tab3, text="This is Tab 3").pack(pady=10)
+
+# Pack the notebook widget
+notebook.pack(expand=1, fill="both")
+
+# Start the main loop
+root.mainloop()
