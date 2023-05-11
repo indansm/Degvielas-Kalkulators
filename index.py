e1dd89e93f4c5a132b3f9b0651744cf0452b233b
indansm <118436727+indansm@users.noreply.github.com>
2023-05-11 13:03:57 +0300

    final commit

--- index.py
+++ index.py
@@ -6,10 +6,9 @@ root = tk.Tk()
 root.title("Degvielas Kalkulators")
 
 def paterins():
-    degv = float(degvieldaudz.get())
-    km = float(km_nobr.get())
+    degv = float(degvieldaudz.get().replace(",", "."))
+    km = float(km_nobr.get().replace(",", "."))
     pat=(degv*100)/km
-    print(pat)
     Degvielas_pat_sk.config(text=f"{pat:.2f} l/100")
 
 # Laiks patērēts atkļūdošanā = 2h
@@ -17,16 +16,16 @@ def paterins():
 
 
 def nobrAtt():
-    degv = float(degvdaudz2_ievade.get())
-    pat = float(Degv_pat_sk2.get())
+    degv = float(degvdaudz2_ievade.get().replace(",", "."))
+    pat = float(Degv_pat_sk2.get().replace(",", "."))
     km_nobraucamais = (degv/pat)*100
 
     KM_sk.config(text=f"{km_nobraucamais:.2f} km")
 
 
 def DegvDaudzums():
-    pat = float(Degv_pat_sk.get())
-    km = float(km_sk.get())
+    pat = float(Degv_pat_sk.get().replace(",", "."))
+    km = float(km_sk.get().replace(",", "."))
     degv=(pat*km)/100
     degvdaudz.config(text=f"{degv:.2f} L")
 
